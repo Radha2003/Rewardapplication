196c69862d807652de6ef59b585b2a104c487e4c
Radha2003 <46052187+Radha2003@users.noreply.github.com>
2026-03-28 15:16:45 -0400

    my initial commit

--- src/main/java/com/example/controller/RewardsController.java
+++ src/main/java/com/example/controller/RewardsController.java
@@ -1,5 +1,23 @@
 package com.example.controller;
-
+import org.springframework.web.bind.annotation.*;
+@RestController
+@RequestMapping("/api")
 public class RewardsController {
-
+	 @GetMapping("/getrewards/customer")
+	    public int  home(int amt) {
+		 
+		 
+		for(int i=1;i<=3;i++)
+		{switch (amt)
+			{
+		case amt  between 0 and 100 :
+			
+			rewardpoints=1*50;
+			}
+		
+		case amt between 100 and above 
+		{rewardpoint=rewardpoints+2*50;}
+			return rewardpoints;
+	       
+	    }
 }
